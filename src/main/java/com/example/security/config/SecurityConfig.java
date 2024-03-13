6ca5ce932a1356a9a9303c3360552f289a4052a8
ShimFFF <shimseawon1510@gmail.com>
2024-03-14 02:20:31 +0900

    :sparkles: Feat: add securityFilterChain method in SecurityConfig.java

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // IoC 빈(bean)을 등록
+@EnableWebSecurity // 필터 체인 관리 시작 어노테이션
+public class SecurityConfig{
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*
+    * 이전 방식  : WebSecurityConfigurerAdapter를 상속하고 configure매소드를 오버라이딩하여 설정
+    * 지금 방식 : SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
+    * */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests((authorize) ->
+                authorize
+                        .requestMatchers("/members/**").authenticated() // 인증만 되면 들어갈 수 있는 주소
+                        //.requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+                        //.requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and hasRole('ROLE_USER')")
+                        .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // ROLE_ADMIN만 들어갈 수 있는 주소
+                        .anyRequest().permitAll() // 그 외의 주소는 모두 허용
+                        // .anyRequest().authenticated() // 그 외의 주소는 모두 인증을 받아야 들어갈 수 있음
+        );
+        return http.build();
+    }
+}
